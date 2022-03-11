2885eb2fc8e6b3c1e2fb8e2e67635f89602ed4e1
MaeJezriel <201810487@gordoncollege.edu.ph>
2022-03-11 14:49:13 +0800

    up

--- src/app/edit/edit.page.ts
+++ src/app/edit/edit.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../crud.service';
 import { ActivatedRoute, Router } from "@angular/router";
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit',
@@ -16,7 +17,8 @@ export class EditPage implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private crud: CrudService
+    private crud: CrudService,
+    private modalCtrl: ModalController
   ) { 
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
 
@@ -28,10 +30,16 @@ export class EditPage implements OnInit {
 
   ngOnInit() { }
 
+  public dismiss(): void {
+    this.modalCtrl.dismiss();
+ }
+
+
   onUpdate() {
      this.crud.updateUser(this.id, this.nameVal, this.emailVal).then(() => {
         this.router.navigate(['/home']);
      })
   }
 
+ 
 }
